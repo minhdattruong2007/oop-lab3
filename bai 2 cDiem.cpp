db1ac03a86d6eba33196bc950c520a2e2c7ed89b
minhdattruong2007 <minhdattruong2007@gmail.com>
2026-04-24 17:49:04 +0700

    Add cDiem class with basic geometric operations

---
+++ bai 2 cDiem.cpp
@@ -0,0 +1,44 @@
+#include "cDiem.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+cDiem::cDiem() {
+    x = 0;
+    y = 0;
+}
+
+void cDiem::Nhap() {
+    cin >> x >> y;
+}
+
+void cDiem::Xuat() {
+    cout << "(" << x << ", " << y << ")";
+}
+
+void cDiem::TinhTien(double dx, double dy) {
+    x += dx;
+    y += dy;
+}
+
+void cDiem::Quay(double goc) {
+    double rad = goc * 3.141592653589793 / 180;
+
+    double xNew = x * cos(rad) - y * sin(rad);
+    double yNew = x * sin(rad) + y * cos(rad);
+
+    x = xNew;
+    y = yNew;
+}
+
+void cDiem::PhongTo(double k) {
+    x *= k;
+    y *= k;
+}
+
+void cDiem::ThuNho(double k) {
+    x /= k;
+    y /= k;
+}
+
